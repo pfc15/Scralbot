319043cfff2c5b7fb7515d2ee6574049aaf29d5d
pfc15 <pedrofcrux@gmail.com>
2024-03-29 17:47:25 -0300

    primeira jogada funcionando, segunda não ta

--- Tabuleiro.cpp
+++ Tabuleiro.cpp
@@ -6,9 +6,9 @@ using namespace std;
 
 struct item;
 struct item {
-            char letra;
-            int pos_palavra;
-            int pos_grafo;
+    char letra;
+    int pos_palavra;
+    int pos_grafo;
 };
 
 
@@ -26,11 +26,11 @@ class Jogo
         
 
     Jogo(vector<string>  nomes){
+        // inicializando jogadores
         std::srand(std::time(nullptr));
         for (string nome:nomes){
             Jogador player = Jogador(nome);
             jogadores.push_back(player);
-            cout << "nome: " <<player.nome << "; pecas: [" << player.pecas << "]" << endl; 
         }
 
         // init da matiz
@@ -73,11 +73,10 @@ class Jogo
         return grafo;
     };
 
-
     void mostrar_tabuleiro(){
-        cout << "\033[2J\033[1;1H";
+        //cout << "\033[2J\033[1;1H";
         for (Jogador j: jogadores){
-            cout <<"nome: " << j.nome << "peças: [" << j.pecas << "]" << endl;
+            cout <<"nome: " << j.nome << "; peças: [" << j.pecas << "]" << endl;
         }
         int cont =0;
         char l = 'a';
@@ -108,12 +107,14 @@ class Jogo
     }
 
     int dfs(int u, string caminho, vector<queue<item>> grafo, string pecas){
+        
+        // visitando o  nó
         caminho += grafo[u].front().letra;
         tentativas.push_back(caminho);
         if (dic.procura(caminho)>=0){
             retorno.emplace(caminho);
         }
-        
+
         queue<item> fila = grafo[u];
         fila.pop();
         int index;
@@ -129,35 +130,79 @@ class Jogo
             }
             
         }
-        return 1;
-
+        return tentativas.size();
     }
 
     void movimento(Jogador j){
         string pecas = jogadores.at(0).pecas;
-        pecas += 'a';
+        pair<int, int> pos = escolha_ancora();
+        char ancora = tabuleiro_pecas.at(pos.first).at(pos.second);
+        if (ancora != ' '){
+            pecas += ancora;
+        }
         vector<queue<item>>  grafo = cria_grafo(pecas);
 
         retorno = {};
-
+        int tentaivas_quant =0;
         for (int i=0;i<8;i++){
             string aux = pecas;
             aux.erase(i,1);
-            dfs(i,string(""), grafo, aux);
+            tentaivas_quant += dfs(i,string(""), grafo, aux);
         }
-        cout << "dfs completa!" << endl;
-        pair<int, int> pos = {7, 6};
-        posicionar(retorno.top(), pos, 0);
+        cout << "dfs completa! " << tentaivas_quant << " tentativas" << endl;
+
+        string entrega = retorno.top();
+        cout << "1palavra: " << entrega << "topo: " << retorno.top() << endl;
+        int direcao;
+        if (tabuleiro_pecas.at(pos.second).at((pos.first+1)%15)==' '&&tabuleiro_pecas.at(pos.second).at((pos.first-1)%15)==' '){
+            direcao =0;
+        } else {
+            direcao=1;
+        }
+
+        cout << "2palavra: " << entrega << "topo: " << retorno.top()<< endl;
+        int x = pos.first;
+        int y = pos.second;
+        cout << "3palavra: " << entrega << "topo: " << retorno.top() << endl;
+        for (int i=0;i<entrega.size();i++){
+            cout << "4palavra: " << entrega << "topo: " << retorno.top() << endl;
+            tabuleiro_pecas.at(y).at(x) = entrega.at(i);
+            if (direcao ==1){
+                y++;
+            } else if (direcao ==0){
+                x++;
+            }
+        }
+        
         
     }
 
-    void posicionar(string palavra, pair<int, int> pos, int direcao){
-        for (char letra:palavra){
-            if (direcao ==0){
-                tabuleiro_pecas.at(pos.first).at(pos.second) = letra;
-                pos.second++;
+    pair<int, int> escolha_ancora(){
+        pair<int, int> retorno; 
+        for (int x=0;x<tabuleiro_pecas.size();x++){
+            for (int y=0;y<tabuleiro_pecas.size();y++){
+                if (tabuleiro_pecas.at(y).at(x) == 'a'|| tabuleiro_pecas.at(y).at(x) == 'e' || tabuleiro_pecas.at(y).at(x) == 'i' || tabuleiro_pecas.at(y).at(x) == 'o' || tabuleiro_pecas.at(y).at(x) == 'u'){
+                    retorno = {x, y};
+                    return retorno;
+                }
             }
+        }
+        retorno = {7, 6};
+        return retorno;
+    }
+
+    void posicionar(pair<int, int> pos, int direcao){
+        int cont =0; 
+        cout << cont << "ola:0 " << retorno.top() << endl;
+        for (char letra:retorno.top()){
             
+            cont ++;
+            tabuleiro_pecas.at(pos.second).at(pos.first) = letra;
+            if (direcao ==1){
+                pos.second++;
+            } else if (direcao ==0){
+                pos.first++;
+            }
         }
     }
 
@@ -168,5 +213,10 @@ int main(){
     vector<string> lista = {string("pedro"), string("pablo")};
     Jogo j = Jogo(lista);
     j.movimento(j.jogadores.at(0));
+    j.mostrar_tabuleiro();
+    int i;
+    cout << "digite 1 para prox jogador jogar: " ;
+    cin >> i;
+    j.movimento(j.jogadores.at(1));
     j.mostrar_tabuleiro();
 }
